4914f2a6c451b2c58281b10b3e7f8e665e6eb1d6
Cameron <hackwithcameron@gamil.com>
2019-10-16 11:50:01 -0700

    JavaScripit String Concatenate

--- JavaScript/Basic_JavaScript_2.js
+++ JavaScript/Basic_JavaScript_2.js
@@ -6,3 +6,9 @@ var B = B.fontcolor("green");
 document.write(B);
 document.getElementById('button').innerHTML = B;
 }
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
