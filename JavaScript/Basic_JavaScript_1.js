73b502744407a41d639397f404b4b7ad00d863cf
Cameron <hackwithcameron@gamil.com>
2019-10-16 10:55:17 -0700

    JavaScript basics
    various JavaScript actions

---
+++ JavaScript/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+
+var A = "Hey, " + "how are you?", B = "Doing great, thanks!";
+var A = A.fontcolor("orange"), B = B.fontcolor("green");
+var express = (100/5)
+
+
+document.write(A);
+document.write(B);
+document.write(express);
