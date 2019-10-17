95cd86550c0a347a8ec8a16c2a3ae926b633d0df
Cameron <hackwithcameron@gamil.com>
2019-10-17 15:58:09 -0700

    If Else
    practice with if and else statements

--- JavaScript/Basic_JavaScript_7.js
+++ JavaScript/Basic_JavaScript_7.js
@@ -1,16 +1,31 @@
-function Vehicle (Make, Model, Year, Color){
-    this.Vehicle_Make = Make;
-    this.Vehicle_Model = Model;
-    this.Vehicle_Year = Year;
-    this.Vehicle_Color = Color;
+var a = 25;
+
+function Right() {
+    var b = 34;
+    var result = a + b;
+    console.log(result);
+    document.getElementById("log").innerHTML = result;
 }
 
-var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
-var Emily = new Vehicle("Jeep", "Trailhawk", 2019, "White and Black");
-var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function Not_Right() {
+    var c = 18;
+    var result = b + c;
+    document.getElementById("log2").innerHTML = result;
+    console.log(result);
+}
 
-function myFunction () {
-    document.getElementById("Keywords_and_Constructors").innerHTML = 
-    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
-    " manufactured in " + Erik.Vehicle_Year;
+function get_time() {
+    var d = new Date();
+    document.getElementById("time").innerHTML = d.getHours() + ":" + d.getMinutes();
+}
+
+function age_check() {
+    var age = document.getElementById("age").value;
+    if (age > 17) {
+        document.getElementById("enter_id").innerHTML = "You are over 18yrs old.";
+    }
+    else {
+        document.getElementById("enter_id").innerHTML = "You are under 18yrs old.";
+    }
+    console.log(age);
 }
